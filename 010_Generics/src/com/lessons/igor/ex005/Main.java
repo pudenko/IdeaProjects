d456865efdc10157b6d1eb5b79d4859ac7504a3d
Pudenko <30345231+pudenko@users.noreply.github.com>
2020-11-15 17:33:48 +0200

    Update Main.java

--- 010_Generics/src/com/lessons/igor/ex005/Main.java
+++ 010_Generics/src/com/lessons/igor/ex005/Main.java
@@ -3,6 +3,6 @@ package com.lessons.igor.ex005;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Теперь я в Githab!!!");
-
+ System.out.println("TEST!!!");
     }
 }
