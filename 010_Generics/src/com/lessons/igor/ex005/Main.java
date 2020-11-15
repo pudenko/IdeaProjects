0dc89fa48ad401a480554fc2f4cefa85bcd888c8
pudikq@gmail.com <pudikq@gmail.com>
2020-11-15 17:32:28 +0200

    Generics

--- 010_Generics/src/com/lessons/igor/ex005/Main.java
+++ 010_Generics/src/com/lessons/igor/ex005/Main.java
@@ -3,5 +3,7 @@ package com.lessons.igor.ex005;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Теперь я в Githab!!!");
+        System.out.println("Теперь я в Githab!!!");
+        System.out.println("Теперь я в Githab!!!");
     }
 }
