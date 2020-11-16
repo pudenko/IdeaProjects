b1b9818c99d337365bc16e737f03240476d2ea02
i.pudenko <i.pudenko@etgar.com.ua>
2020-11-16 12:29:16 +0200

    test2

--- 010_Generics/src/com/lessons/igor/ex005/Main.java
+++ 010_Generics/src/com/lessons/igor/ex005/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Теперь я в Githab!!!");
         System.out.println("TEST!!!");
+        System.out.println("TEST!!!");
+        System.out.println("TEST!!!");
+        System.out.println("TEST!!!");
     }
 }
